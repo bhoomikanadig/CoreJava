b0ca8a6aca657a3fb1642b696ac623a9ba480483
Bhoomika Nadig <75989492+bhoomikanadig@users.noreply.github.com>
2022-06-11 19:14:23 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,25 @@
+package com.xworkz.corejava.DataType;
+
+public class Datatype {
+
+	public static void main(String[]args){
+
+        byte varByte = 3 ;
+        char varChar = 'd' ;
+        int varInt = 4 ;
+        long  varLong = 1234455 ;
+        short  varShort = 23345;
+        float  varFloat = 50.90f ;
+        double  varDouble = 98.5674d ;
+        boolean  varBool = true ; 
+
+        System.out.println("varByte :"+varByte) ;
+        System.out.println("varChar :"+varChar) ;
+        System.out.println("varInt :"+varInt) ;
+        System.out.println("varLong :"+varLong) ;
+        System.out.println("varShort :"+varShort) ;
+        System.out.println("varFloat :"+varFloat) ;
+        System.out.println("varDouble :"+varDouble) ;
+        System.out.println("varBool :"+varBool) ;
+}
+}
